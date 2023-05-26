5123420efbb293a4fceced38918e4b0152a33d83
unknown <nguyenquocthinhit@gmail.com>
2023-05-27 01:02:45 +0700

    test light/dark theme

--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -1,1 +1,29 @@
 //setup my theme in here
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData lightTheme = ThemeData(
+  textTheme: TextTheme(
+    headlineMedium: TextStyle(
+      color: Colors.black
+    )
+  ),
+    fontFamily: GoogleFonts
+        .roboto()
+        .fontFamily,
+    colorScheme: const ColorScheme.light(
+        background: Colors.red,
+        brightness: Brightness.light,
+        onSurface: Colors.amber,
+        surface: Colors.green
+    ),
+    useMaterial3: true);
+
+ThemeData darkTheme = ThemeData(
+    fontFamily: GoogleFonts
+        .roboto()
+        .fontFamily,
+    colorScheme: const ColorScheme.dark(
+      background: Colors.black12,),
+    useMaterial3: true);
--- lib/network/api/example_api.dart
+++ lib/network/api/example_api.dart
@@ -1,14 +1,15 @@
 import 'package:http/http.dart' as http;
 import 'package:travel_app/network/repositories/example_repository.dart';
+
 import '../utils/response.dart';
+
 // Example: request API
-// How to use: ExampleAPI.getDataExample()
-// How to use: ExampleAPI.postDataExample(your_data)
+// How to use: ExampleAPI().getDataExample()
+// How to use: ExampleAPI().postDataExample(your_data)
 
+const String EXAMPLE_URL = "http://exampleAPI.com";
+class ExampleAPI extends ExampleRepo {
 
-const String EXAMPLE_URL = "http://example.com";
-
-class ExampleAPI implements ExampleRepo {
   @override
   Future<Object> getDataExample() async {
     try {
@@ -35,5 +36,9 @@ class ExampleAPI implements ExampleRepo {
     } catch (e) {
       return Failure(strError: "error");
     }
+  }
+
+  ExampleAPI(){
+   //init config api here!
   }
 }
